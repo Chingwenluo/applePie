86900ec5178ac4c1f74b2be62a52ab298e00c46b
ChingWen <fsvnxwx@gmail.com>
2021-01-11 00:04:37 +0800

    update

--- applePie/Game.swift
+++ applePie/Game.swift
@@ -6,31 +6,51 @@
 //
 
 import Foundation
+import AVFoundation
 
 struct Game {
-    var word: String
-    var incorrectMovesRemaining: Int
-    var guessedLetters: [Character]
-    
-    mutating func playerGuessed (letter: Character) {
-         guessedLetters.append(letter)
-        if !word.contains(letter){
-           incorrectMovesRemaining -= 1
+    var word1: String
+    var incorrectMovesRemainingPlayer1: Int
+    var guessedPlayer1: [Character]
+   
+    mutating func player1Guessed (letter: Character) {
+        guessedPlayer1.append(letter)
+        if !word1.contains(letter){
+           incorrectMovesRemainingPlayer1 -= 1
+            speek()
+          
          }
+        
+       }
+    
+    
+    func speek(){
+           
+           let speechUtterance = AVSpeechUtterance(string: "oh oh 猜錯了")
+           speechUtterance.voice = AVSpeechSynthesisVoice(language: "zh-TW")
+           speechUtterance.rate = 0.5
+           speechUtterance.postUtteranceDelay = 50
+           let synthesizer = AVSpeechSynthesizer()
+           synthesizer.speak(speechUtterance)
        }
     
     var formattedWord: String  {
         var guessedWord = ""
-        for letter in word{
-            if guessedLetters.contains(letter)  {
+      
+        for letter in word1{
+            if guessedPlayer1.contains(letter)  {
                 guessedWord += "\(letter)"
+            
             }
             else {
                 guessedWord += "_"
             }
          }
+        
         return guessedWord
     }
+    
+    
     
 }
 
--- applePie/ViewController.swift
+++ applePie/ViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import AVFoundation
+import Foundation
 
 
 class ViewController: UIViewController {
@@ -14,104 +16,179 @@ class ViewController: UIViewController {
     @IBOutlet weak var correctWordLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet var letterButtons: [UIButton]!
+    @IBOutlet weak var scoreCalculationLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    
+
+    var listOfWords = ["book", "swift", "apple","peter", "bug", "program", "orange","pen","water","tv","hot","job",]
    
-    //單字
-    var listOfWords = ["buccaneer", "swift", "glorious",
-    "incandescent", "bug", "program"]
-   
-    //答對數量
+    var guessRight = 0 {
+        didSet {
+          newRound()
+         }
+      }
+    
     var totalWins = 0  {
       didSet {
         newRound()
        }
     }
     
-    //答錯數量
+   
     var totalLosses = 0  {
         didSet {
      newRound()
-}
+        }
     }
     
-
-    let incorrectMovesAllowed = 7
-    
+ 
+    let incorrectMovesAllowedPlayer1 = 7
     var currentGame: Game!
-  
+    var game = 0
+    var gameTimer:Timer?
+    var timerCount = 0
+    var totalTime = 0
+    var select = 0
+    
+    
+    @objc func timerSet() {
+        
+        timerCount += 1
+        let mins =  timerCount/60
+        let seconds = timerCount % 60
+        self.timeLabel.text = " 時間 \(String(format:"%.2d", mins)):\(String(format:"%.2d", seconds))"
+        totalTime += 1
+    }
+        
+    
     func newRound() {
-
+       
       if  listOfWords.isEmpty == false {
-
-        let newWord = listOfWords.removeFirst()
-        currentGame = Game(word: newWord, incorrectMovesRemaining: incorrectMovesAllowed, guessedLetters: [])
+      
+        let newWord = listOfWords.removeLast()
+        currentGame = Game(word1: newWord, incorrectMovesRemainingPlayer1: incorrectMovesAllowedPlayer1, guessedPlayer1: [])
+  
             enableLetterButtons(_enable: true)
-            updateUI()
-
+        updateUI1()
         }
-        else {
+
+        
+      else {
             enableLetterButtons(_enable: false)
-            if  listOfWords.isEmpty == true {
-
-                listOfWords = ["buccaneer", "swift", "glorious","incandescent", "bug", "program"]
-
-                }
 
             }
 
     }
 
     func enableLetterButtons(_enable: Bool)  {
-        
+
         for button in letterButtons  {
-            button.isEnabled = true
-            
+            button.isEnabled = _enable
+
        }
     }
     
-    func updateUI() {
+    func updateUI1() {
         var letters = [String]()
         for letter in currentGame.formattedWord {
             letters.append(String(letter))
         }
+
         let wordWithSpacing = letters.joined (separator:" ")
+
+     
+        switch select {
+        case 0:
+            treeImageView.image = UIImage(named: "Tree \(currentGame.incorrectMovesRemainingPlayer1)")
+        case 1:
+            treeImageView.image = UIImage(named: "orange \(currentGame.incorrectMovesRemainingPlayer1)")
+        case 2:
+            treeImageView.image = UIImage(named: "Strawberry \(currentGame.incorrectMovesRemainingPlayer1)")
+        default:
+            break
+        }
+    
+         
+      
+        
+        
+       
         correctWordLabel.text = wordWithSpacing
-        scoreLabel.text = "正解: \(totalWins),誤答: \(totalLosses)"
-        treeImageView.image = UIImage(named: "Tree \(currentGame.incorrectMovesRemaining)")
+        scoreLabel.text = "猜對: \(totalWins),猜錯: \(totalLosses)"
+        scoreCalculationLabel.text = "得分: \(guessRight)"
+       endgame()
+
+        }
+        
+    
+    func endgame(){
+        if totalWins == 5 {
+            gameTimer?.invalidate()
+
+            let alert = UIAlertController(title: "花費時間", message: "\(totalTime)秒"  , preferredStyle: .alert)
+                    let action = UIAlertAction(title: "結束", style: .default) { (_) in
+                    }
+                        alert.addAction(action)
+                        present(alert, animated: true, completion: nil)
+                }
     }
     
+
     
     func updateGameState() {
-        if currentGame.incorrectMovesRemaining == 0 {
-            
+        if currentGame.incorrectMovesRemainingPlayer1 == 0 {
             totalLosses += 1
-        
+           
+        } else if currentGame.incorrectMovesRemainingPlayer1 == 0 {
             
-        }
-        else if currentGame.word == currentGame.formattedWord {
-            
+        }  else if currentGame.word1 == currentGame.formattedWord {
+           
             totalWins += 1
+            guessRight += 10
            
             
         }
         else {
-            updateUI ()
+            updateUI1 ()
             
         }
     }
-
-    
+        
     @IBAction func buttonTapped(_ sender: UIButton) {
         
         sender.isEnabled = false
         let letterString = sender.title(for: .normal)!
         let letter = Character(letterString.lowercased())
-        currentGame.playerGuessed(letter:letter)
+    
+        currentGame.player1Guessed(letter:letter)
         updateGameState()
-       
+             
     }
+    
+
+    @IBAction func chioseSegmented(_ sender: UISegmentedControl) {
+        
+        select = sender.selectedSegmentIndex
+        switch select {
+        case 0:
+            treeImageView.image = UIImage(named: "Tree 7")
+        case 1:
+            treeImageView.image = UIImage(named: "orange 7")
+        case 2:
+            treeImageView.image = UIImage(named: "Strawberry 7")
+        default:
+            break
+        }
+        
+    }
+    
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        gameTimer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(self.timerSet), userInfo: nil, repeats: true)
+       
         newRound()
         // Do any additional setup after loading the view.
     }
